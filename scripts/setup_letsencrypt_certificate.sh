620a15a742c1093ad5ca63bd6f0151c79920d51e
victor <victorblanco.21@hotmail.com>
2025-11-12 09:05:36 +0000

    comando autentificacion certbot y finalizo

--- scripts/setup_letsencrypt_certificate.sh
+++ scripts/setup_letsencrypt_certificate.sh
@@ -4,15 +4,21 @@ set -ex
 #importamos el archivo .env
 source .env
 
+# Copiamos la plantilla del archivo VirtualHost en el servidor
+cp ../conf/000-default.conf /etc/apache2/sites-available
+
+# Configuramos el ServerName en el VirtualHost
+sed -i "s/PUT_YOUR_CERTBOT_DOMAIN_HERE/$CERTBOT_DOMAIN/"  /etc/apache2/sites-available/000-default.conf
+
 # Instalamos snap
 snap install core
 snap refresh core
 
 #Eliminamos instalaciones previas de cerbot
-apt remove cerbot -y
+apt remove certbot -y
 
 # Instalamos cerbot
-snap install --classic cerbot
+snap install --classic certbot
 
 # Solicitamos el certificado a Let-s Encrypt
-cerbot --apace -m  $CERTBOT_EMAIL --agree-tos --no-eff-email -d $CERTBOT_DOMAIN
+certbot --apache -m $CERTBOT_EMAIL --agree-tos --no-eff-email -d $CERTBOT_DOMAIN --non-interactive
